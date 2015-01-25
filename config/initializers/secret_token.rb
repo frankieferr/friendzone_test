8a4c7e765f0b4622f59a44d880542bc4ea83d954
frankieferr <frankstar_frankilicious@hotmail.com>
2015-01-26 08:36:52 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriendzoneTest::Application.config.secret_key_base = '50949a8a41e32f6bf5765d14b564bf48d3444fc0e5efbc1d5a017da82de5cc2b5658a293662633624466a4b997e43a3f52f118093f65d6a351e1f3e74f4b9d95'
